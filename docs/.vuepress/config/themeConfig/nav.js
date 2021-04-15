908ddd017236f77ad314882a2a73c33d6a4c26e7
风铃 <fengling@gaoding.com>
2021-04-16 00:50:54 +0800

    目录调整,图片压缩,新文档

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -8,14 +8,14 @@ module.exports = [
       {
         text: '基础知识点',
         items: [
-          { text: 'JavaScript', link: '/pages/a0057b/' },
+          { text: 'JavaScript', link: '/pages/4456e3/' },
           { text: 'ES6', link: '/pages/a0057b/' },
           { text: 'HTML与CSS', link: '/pages/a0057b/' },
           { text: 'JS设计模式', link: '/pages/a0057b/' },
         ],
       },
       {
-        text: '博文',
+        text: '我的博文',
         items: [
           { text: 'Vue', link: '/pages/a0057b/' },
           { text: 'React', link: '/pages/a0057b/' },
@@ -26,7 +26,7 @@ module.exports = [
     ],
   },
   {
-    text: '算法题解',
+    text: '算法|试题',
     link: '/algorithm/',
     items: [
         { text: '排序算法', link: '/pages/a0057b/' },
@@ -35,20 +35,18 @@ module.exports = [
     ],
   },
   {
-    text: '第三方服务',
+    text: '插件|框架',
     link: '/services/',
     items: [
         { text: 'Vue', link: '/pages/a0057b/' },
         { text: 'React', link: '/pages/a0057b/' },
-        { text: 'Webpack', link: '/pages/a0057b/' },
-        { text: 'Eslint与Babel', link: '/pages/a0057b/' },
-        { text: 'Nodejs', link: '/pages/a0057b/' },
     ],
   },
   {
-    text: '工具使用',
+    text: '项目|工具',
     link: '/tools/',
     items: [
+      { text: '项目搭建', link: '/pages/docs/' },
       { text: '技术文档', link: '/pages/docs/' },
       { text: 'Git技巧', link: '/pages/git/' },
     ],
